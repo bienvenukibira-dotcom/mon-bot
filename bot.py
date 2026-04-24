6305267bf68eac55f89c04a1b6dab3f65e30e908
bienvenukibira-dotcom <bienvenukibira@gmail.com>
2026-04-24 21:25:07 +0300

    Refactor bot.py to change reply message

--- bot.py
+++ bot.py
@@ -1,9 +1,6 @@
 import telebot
 import os
-import random
-from PIL import Image, ImageDraw
 
-# 🔑 récupérer le token depuis Render
 TOKEN = os.getenv("8759628647:AAH6XfSmHCHQgt-b4ODJAmgQHE40HGZaCcw")
 
 if not TOKEN:
@@ -12,7 +9,7 @@ if not TOKEN:
 
 bot = telebot.TeleBot(TOKEN)
 
-# 🔥 corrige erreur 409
+# corrige erreur 409
 bot.delete_webhook()
 
 @bot.message_handler(commands=['start'])
@@ -21,18 +18,7 @@ def start(message):
 
 @bot.message_handler(func=lambda message: True)
 def reply(message):
-    signal = random.choice(["CALL", "PUT"])
-
-    img = Image.new('RGB', (400, 300), color='black')
-    draw = ImageDraw.Draw(img)
-
-    color = (0,255,0) if signal=="CALL" else (255,0,0)
-    draw.text((150,130), signal, fill=color)
-
-    img.save("signal.png")
-
-    with open("signal.png", "rb") as photo:
-        bot.send_photo(message.chat.id, photo)
+    bot.send_message(message.chat.id, "Je réponds 👍")
 
 print("Bot lancé...")
 bot.infinity_polling()
